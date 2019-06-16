619d51baafa9834b6bc7318bc94f88bdc5ca3929
Сергей Пасиченко <50148048+loki911@users.noreply.github.com>
2019-06-16 23:50:54 +0300

    Create Triangle.java

---
+++ oop/hw3/Triangle.java
@@ -0,0 +1,39 @@
+package oop.hw3;
+
+public class Triangle Triangle extends Figure implements Perimetr {
+    public Point a;
+    public Point b;
+    public Point c;
+
+    public Triangle(Point a, Point b, Point c) {
+        super();
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    @Override
+    public boolean isTriangle() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle {" +
+                "a = " + a +
+                ", b = " + b +
+                ", c = " + c +
+                '}';
+    }
+
+    @Override
+    public double getPerimetr() {
+        return a.distance(b) + b.distance(c) + c.distance(a);
+    }
+
+    @Override
+    public double getArea() {
+        double semiPerimeter = getPerimetr() / 2;
+        return Math.sqrt(semiPerimeter * (semiPerimeter - a.distance(b)) * (semiPerimeter - b.distance(c)) * (semiPerimeter - c.distance(a)));
+    }
+}
