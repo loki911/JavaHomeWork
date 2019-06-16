7b19de86c97e6c720fc7544a591dcad5836f5f87
Сергей Пасиченко <50148048+loki911@users.noreply.github.com>
2019-06-16 19:24:25 +0300

    Create Truck.java

---
+++ collections/hw2/Truck.java
@@ -0,0 +1,23 @@
+package collections;
+
+public class Truck extends Car {
+
+    public Truck(String brand, String model, Integer year, String type, String color, Integer speed) {
+        super(brand, model, year, type, color, speed);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
