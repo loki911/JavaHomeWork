f6f8b07e7b3f90b4315b810dfc75d2c88c737fcd
Сергей Пасиченко <50148048+loki911@users.noreply.github.com>
2019-06-16 21:19:14 +0300

    Create PushkinCounter.java

---
+++ io/hw/PushkinCounter.java
@@ -0,0 +1,37 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class PushkinCounter {
+    private static String PATH = "C:\\Users\\Loki\\IdeaProjects\\JavaHomeWork\\src\\io";
+
+    public static Map<Character, Integer> readText() throws IOException {
+        Map<Character, Integer> letters = new TreeMap<>();
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(PATH + "\\anchar"))){
+            String line;
+            while ((line = reader.readLine()) != null) {
+                for (char letter : line.replaceAll("\\s+", "").toLowerCase().toCharArray()) {
+                    if (letters.get(letter) == null) {
+                        letters.put(letter, 1);
+                    } else {
+                        letters.put(letter, letters.get(letter) + 1);
+                    }
+                }
+                line.toCharArray();
+            }
+        }
+        try (BufferedWriter out = new BufferedWriter(new FileWriter(PATH + "\\letters.txt"))){
+            for (Map.Entry<Character, Integer> entry : letters.entrySet()) {
+                out.write(entry.getKey() + " - " + entry.getValue() + "\n");
+            }
+        }
+        return letters;
+    }
+}
