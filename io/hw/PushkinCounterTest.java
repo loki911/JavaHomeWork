b562839498f2f75bf5f49dd097e655078025c720
Сергей Пасиченко <50148048+loki911@users.noreply.github.com>
2019-06-16 21:20:00 +0300

    Create PushkinCounterTest.java

---
+++ io/hw/PushkinCounterTest.java
@@ -0,0 +1,11 @@
+package io;
+
+import java.io.IOException;
+
+import static io.PushkinCounter.readText;
+
+public class PuskinCounterTest {
+    public static void main(String[] args) throws IOException {
+        System.out.println(readText());
+    }
+}
